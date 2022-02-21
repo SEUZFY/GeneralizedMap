7ca895edcaad5f5d0b448a7faf9e6d26dd2370de
Yitong Xia <xiayitong0630@gmail.com>
2022-02-21 15:20:11 +0100

    update input function and test output function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-﻿// some standard libraries that are helpfull for reading/writing text files
+// some standard libraries that are helpfull for reading/writing text files
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -7,44 +7,43 @@
 
 #include "Gmap.h"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, const char * argv[]) {
 
-    // Example how to read vertices from an obj file :
-    std::string filename = "/cube.obj"; // could read from the main() parameters
-    std::string file_in = DATA_PATH + filename;
+    std::string file_in = "./cube.txt";
+    std::ifstream ifs;
+    ifs.open(file_in);
 
-    std::ifstream stream_in;
-    stream_in.open(file_in);
+    bool status=ifs.is_open();
+    std::cout<<"the result is "<<status<<std::endl;
+
     std::vector<Vertex> vertices;
-
-    if (stream_in.is_open()) {
+    std::vector<Face> faces;
+    int vertices_id = 1;
+    if (ifs.is_open()) {
         std::string line;
-        while (getline(stream_in, line)) {
+        while (getline(ifs, line)) {
             std::istringstream iss(line);
             std::string word;
             iss >> word;
             if (word == "v") {
                 std::vector<float> coordinates;
-                while (iss >> word) coordinates.emplace_back(std::stof(word));
-                if (coordinates.size() == 3) vertices.emplace_back(coordinates[0], coordinates[1], coordinates[2]);
-                else vertices.emplace_back(Vertex());
+                while (iss >> word) coordinates.push_back(std::stof(word));
+                if (coordinates.size() == 3) vertices.emplace_back(coordinates[0], coordinates[1], coordinates[2],vertices_id);
+                else vertices.push_back(Vertex());
+                vertices_id++;
+            }
+            else if (word == "f") {
+                std::vector<int> face;
+                while(iss>>word) face.push_back(std::stof(word));
+                if (face.size()==4) faces.emplace_back(vertices[face[0]-1].point,vertices[face[1]-1].point,vertices[face[2]-1].point,vertices[face[3]-1].point);
+
+                else faces.push_back(Face());
             }
         }
     }
-
-    // test the read
-    for (auto& v : vertices)v.print();
-
-
-    // ## Construct generalised map using the structures from Gmap.h ##
-
-    // ## Output generalised map to CSV ##
-
-    // ## Create triangles from the darts ##
-
-    // ## Write triangles to obj ##
+    for (int i = 0; i < faces.size(); ++i) {
+        faces[i].face_ver.print();
+    }
 
     return 0;
-
 }
