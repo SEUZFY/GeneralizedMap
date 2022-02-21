a055e75300f73ec318db21eebf7cc7d287f39a0e
Yitong Xia <xiayitong0630@gmail.com>
2022-02-21 15:12:49 +0100

    update Gmap.h

--- src/Gmap.h
+++ src/Gmap.h
@@ -9,83 +9,80 @@ class Edge;
 class Face;
 class Volume;
 
-/*
-Below you find the basic elements that you need to build the generalised map.
-The main thing you need to fill out are the links between the elements:
-  * the involutions and cells on the Dart
-  * the darts on the cells
-
-One way to do this is by using pointers. eg. define a member on the dart struct like
-
-  Struct Dart {
-    // involutions:
-    Dart* a0 = nullptr;
-    // ...
-
-    // cells:
-    // ...
-
-  };
-
-Then you could create and link Darts like:
-
-  Dart* dart_a = new Dart();
-  Dart* dart_b = new Dart();
-
-  dart_a->a0 = dart_b;
-*/
-
 class Dart {
-public:  
-    // involutions:
-    // ..
-
-    // cells:
-    // ...
-
+    int dart_id;
+    std::vector<Dart *> dart_involutions;
+    Vertex * vertex;
+    Edge *edge;
+    Face *face;
+    Dart():dart_id(0)
+    {}
 };
 
-class Vertex : public Point{
-    // access the coordinates of this vertex:
-    // x, y, z
-    // v[0], v[1], v[2]
-public:
-    // constructor without arguments
-    Vertex() : Point(){}
-
+class Vertex {
+    int vertex_id;
+    Point point;
+    Vertex() : point(Point())
+    {}
     // constructor with x,y,z arguments to immediately initialise the point member on this Vertex.
-    Vertex(const float& x, const float& y, const float& z) : Point(x, y, z){}
-
+    Vertex(const double &x, const double &y, const double &z, const int id) : point(Point(x,y,z)),vertex_id(id)
+    {}
+    void print() const {
+        std::cout << "(" << point[0] << ", " << point[1] << ", " << point[2] << ")" << vertex_id<<std::endl; }
     // a dart incident to this Vertex:
     // ...
-
-    virtual ~Vertex() = default;
-   
-
+    Dart * incident_dart;
 };
 
 class Edge {
-public:
-    // a dart incident to this Edge:
-    // ...
+    int edge_id;
 
-    // function to compute the barycenter for this Edge (needed for triangulation output):
-    // Point barycenter() {}
+  // a dart incident to this Edge:
+
+  // function to compute the barycenter for this Edge (needed for triangulation output):
+  // Point barycenter() {}
+
+};
+class Face_vertices{
+public:
+    int x,y,z,t;
+    Point a,b,c,d;
+    // Face_vertices(const int &x, const int &y, const int &z,const int &t) : x(x), y(y), z(z),t(t)
+    // {}
+    Face_vertices(const Point &a, const Point &b, const Point &c,const Point &d) : a(a), b(b), c(c), d(d)
+    {}
+
+    void print() const {
+        std::cout << "(" << a << ", " << b << ", " << c << ", "<< d << ")" << std::endl; }
+    Face_vertices():x(0), y(0), z(0),t(0)
+    {}
 };
 
-class Face {
-public:
-    // a dart incident to this Face:
-    // ...
 
-    // function to compute the barycenter for this Face (needed for triangulation output):
-    // Point barycenter() {}
+class Face {
+    int face_id;
+    Face_vertices face_ver;
+    std::vector<Point> face_ver_list;
+    Dart * incident_dart;
+
+    Face(const Point &x, const Point &y, const Point &z, const Point &t) : face_ver(Face_vertices(x,y,z,t)),face_id(0)
+    {}
+
+    // Face(const int &x, const int &y, const int &z, const int &t) : face_ver(Face_vertices(x,y,z,t)),face_id(0)
+    // {}
+    Face():face_ver(Face_vertices()),face_id(0)
+    {}
+
+  // a dart incident to this Face:
+  // ...
+
+  // function to compute the barycenter for this Face (needed for triangulation output):
+  // Point barycenter() {}
 
 };
 
 class Volume {
-public:
-    // a dart incident to this Volume:
-    // ...
-
+    int volume_id;
+  // a dart incident to this Volume:
+  // ...
 };
