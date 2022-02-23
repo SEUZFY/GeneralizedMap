fa9c2e0c9331b16e675209527beddb6ec3b373df
Yitong Xia <xiayitong0630@gmail.com>
2022-02-23 15:36:49 +0100

    add test functions.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-// some standard libraries that are helpfull for reading/writing text files
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -9,7 +8,7 @@
 
 int main(int argc, const char * argv[]) {
 
-    std::string file_in = "./cube.obj";
+    std::string file_in = "./cube.txt";
     std::ifstream ifs;
     ifs.open(file_in);
 
@@ -18,8 +17,8 @@ int main(int argc, const char * argv[]) {
 
     std::vector<Vertex> vertices;
     std::vector<Face> faces;
-    int vertices_id = 1;
-    int face_id=1;
+    int vertices_id =0;
+    int face_id=0;
     if (ifs.is_open()) {
         std::string line;
         while (getline(ifs, line)) {
@@ -44,8 +43,23 @@ int main(int argc, const char * argv[]) {
             }
         }
     }
+    for (int i = 0; i < faces.size(); ++i) {
+        std::cout<<" this is face no. "<< faces[i].face_id <<std::endl;
+
+        // for(auto ver:faces[i].edge_list) std::cout<<"the vertex is "<< ver.edge_id <<std::endl;
+
+        // for (int j = 0; j < faces[i].edge_list.size(); ++j) std::cout<<"the edge is "<< faces[i].edge_list[j].edge_start <<", "<<faces[i].edge_list[j].edge_end << std::endl;
+    }
+
     // ## Construct generalised map using the structures from Gmap.h ##
 
+    std::vector<std::vector<Dart *>>darts_list=GeneralizedMap::build_darts(faces);
+    for (auto darts : darts_list) {
+        std::cout<< darts.size() <<std::endl;
+        for (auto dart:darts) {
+            std::cout<<"The dart is "<<dart->dart_id <<", the vertex is "<< dart->vertex->vertex_id<<", the edge is "<<dart->edge->edge_id<<", the face is "<<dart->face->face_id<<std::endl;
+        }
+    }
     // ## Output generalised map to CSV ##
 
     // ## Create triangles from the darts ##
